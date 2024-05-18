cd191ca7ea6c6d23507b0fe57e2b6bdd39ead4a0
Hidayat3118 <muhammadhidayat3118@gmail.com>
2024-05-18 22:42:35 +0800

    login dan register

---
+++ resources/views/login.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kaligrafi Indah</title>
+  <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+
+  <div class="navbar">
+    <a class="logo" href="#logo">Logo</a>
+    <a class="tentang" href="#tentang">Tentang</a>
+    <a href="#layanan">Layanan</a>
+    <a href="#bahan">Bahan</a>
+    <a href="#ornamen">Ornamen</a>
+    <a href="#portofolio">Portofolio</a>
+    <a href="#galeri">Galeri</a>
+    <a href="#testimoni">Testimoni</a>
+    <a class="login" href="#Sign In">Sign In</a>
+
+  </div>
+  <br><br><br><br><br>
+
+  <h1 class="satu">User Login</h1>
+
+  <div class="container">
+    <h1><b> Login</b></h1>
+    <form action="#">
+      <div class="form-group">
+        <label for="username"><b> Username:</b></label>
+        <input type="text" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password"><b>Password:</b></label>
+        <input type="password" id="password" name="password" required>
+      </div>
+      <button type="submit"><b> Sign In</b></button>
+      <br><br>
+      <p>Don't have account? Sign Up</p>
+    </form>
+  </div>
+
+
+  <footer id="navbar">
+    <div class="footer-address">
+      <img src="./Ok.svg" alt="icon ok">
+      <h4>Alamat Workshop Kami</h4>
+    </div>
+
+    
+    <p class="address">Jl. Keramat Raya, RT. 12 No. 55 Kel. Sungai Bilu,<br>
+      Kec. Banjarmasin Timur Kota Banjarmasin, <br>
+      Kalimantan Selatan,70236</p>
+     
+<br>
+<div class="footer-address2">
+  <img src="c.png" alt="">
+  <p>2024 [merk]</p>
+</div>
+
+
+    
+  </footer>
+
+</body>
+
+</html>
