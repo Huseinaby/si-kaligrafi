c127f15cac1349b2b08865580d2b104c24cf2e97
madityapratama <madityapratama12.ap@gmail.com>
2024-05-18 18:26:31 +0800

    pembaruan tampilan

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,41 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/nice-forms.css@0.1.7/dist/nice-forms.css"/>
     <link rel="stylesheet" href="homepage.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Home Page</title>
 </head>
 <body>
-    <div class="navbarutama">
-        <div class="header"> LOGO </div>
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tentang</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Layanan</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Bahan</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Ornamen</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Portofolio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Galeri</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Testimoni</a>
-            </li>
+  <div class="main"> 
+      <header class="menu">
+      <div class="icon">
+        <h2 class="logo">Kaligrafi</h2>
+      </div>
+        <ul>
+          <li><a href="#">Tentang</a></li>
+          <li><a href="#">Layanan</a></li>
+          <li><a href="#">Bahan</a></li>
+          <li><a href="#">Ornamen</a></li>
+          <li><a href="#">Portofolio</a></li>
+          <li><a href="#">Galeri</a></li>
+          <li><a href="#">Testimoni</a></li>
+          <li><a href="#" class="tbl">Sign In</a></li>
         </ul>
+      </header>
+      <div class="content">
+        <h1>KALIGRAFI</h1>
+        <h4>Pembuatan</h4>
+        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis deleniti ullam possimus dolor,<br> quae enim debitis architecto repellendus laboriosam recusandae?<br> Eaque molestias dignissimos ipsam, quia quos consequuntur reiciendis sequi voluptate?</p>
+
+        <button class="btn"><a href="https://wa.me/6285845510008"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16"><path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/></svg> Hubungi Kami</a ></button>
+      </div>
     </div>
-    
-    
 </body>
 </html>
