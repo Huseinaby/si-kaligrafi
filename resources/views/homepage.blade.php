a7effbea77d5edc55d67b2560a0a136df3f9d015
Hidayat3118 <muhammadhidayat3118@gmail.com>
2024-06-22 22:57:55 +0800

    Pesan commit yang menjelaskan perubahan

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -222,6 +222,36 @@
                     <div class="col-md-3">
                         <div class="card">
                             <div class="card-body">
+                                <img src="image/layanan1.jpg" class="card-img-top" alt="Expert Image">
+                                <h6 class="card-title">PVC</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <img src="image/layanan1.jpg" class="card-img-top" alt="Expert Image">
+                                <h6 class="card-title">PVC</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <img src="image/layanan1.jpg" class="card-img-top" alt="Expert Image">
+                                <h6 class="card-title">PVC</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
                                 <img src="image/layanan1.jpg" class="card-img-top" alt="Trustworthy Image">
                                 <h6 class="card-title">PVC</h6>
                                 <p class="card-text">Some quick example text to build on the card title and make up the
