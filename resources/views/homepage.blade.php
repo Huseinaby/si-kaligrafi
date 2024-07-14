fc19b848539be5b38511e7b3fca681e6cba54f55
Abu Husein <huseinabu01@gmail.com>
2024-07-15 00:43:12 +0800

    isi homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -76,7 +76,8 @@
 <div class="content">
     <h2>KALIGRAFI</h2>
     <h3>Pembuatan</h3>
-    <p class="textutama"> Percantik masjid Anda dengan sentuhan seni kaligrafi islami dari kami <br> dan rasakan keindahan
+    <p class="textutama"> Percantik masjid Anda dengan sentuhan seni kaligrafi islami dari kami <br> dan rasakan
+        keindahan
         yang membawa kedamaian dan keberkahan.</p>
     <button class="btn"><a href="https://wa.me/6285845510008?text=Halo%20Mas%20Bro!"><svg
                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -115,8 +116,9 @@
                                     d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5q0 .807-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33 33 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935m10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
                             </svg></h5>
                         <h6 class="card-title">Profesional</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">Kami berkomitmen memberikan layanan kaligrafi masjid dengan standar
+                            profesional yang tinggi, memastikan setiap proyek dikerjakan dengan presisi dan keindahan.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -129,8 +131,8 @@
                                     d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4m9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
                             </svg></h5>
                         <h6 class="card-title">Berpengalaman</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">Dengan pengalaman bertahun-tahun dalam seni kaligrafi masjid, kami memiliki
+                            keahlian untuk menciptakan karya yang memukau dan sesuai dengan harapan Anda.</p>
                     </div>
                 </div>
             </div>
@@ -143,8 +145,8 @@
                                     d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5m0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78zM5.048 3.967l-.087.065zm-.431.355A4.98 4.98 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8zm.344 7.646.087.065z" />
                             </svg></h5>
                         <h6 class="card-title">Tenaga Ahli</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">Didukung oleh tenaga ahli yang terlatih dan berkompeten, kami menjamin
+                            setiap detail kaligrafi masjid Anda ditangani dengan ketelitian dan dedikasi tinggi.</p>
                     </div>
                 </div>
             </div>
@@ -157,8 +159,9 @@
                                     d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708" />
                             </svg></h5>
                         <h6 class="card-title">Terpercaya</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">Kami telah dipercaya oleh banyak masjid untuk menghadirkan kaligrafi yang
+                            berkualitas tinggi, menjadikan kepercayaan Anda sebagai prioritas utama kami dalam setiap
+                            proyek.</p>
                     </div>
                 </div>
             </div>
@@ -316,7 +319,8 @@
                                             <div class="isites">{{ $testimoni->isi_testimoni }}</div>
                                             <div class="unique-overlay">
                                                 <div class="unique-content">
-                                                    <div class="unique-text">{{ $testimoni->karya->nama_masjid }}</div>
+                                                    <div class="unique-text">{{ $testimoni->karya->nama_masjid }}
+                                                    </div>
                                                     <br>
                                                     <div class="unique-text">{{ $testimoni->nama_panitia }}</div>
                                                 </div>
